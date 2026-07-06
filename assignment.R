8e6cdd6590fde8b3afed61a26a1c8269556b757d
polinakrupinina-boop <polinakrupinina@gmail.com>
2026-07-06 21:50:22 +0300

    Update assignment PK.R

--- assignment.R
+++ assignment.R
@@ -9,16 +9,18 @@ works <- gutenberg_works()
 # (1) Отберите ряды, в которых gutenberg_author_id равен 65 или 410;
 # после этого выберите два столбца: author, title
 my_data <- works |> 
-  # ваш код здесь
-
-# (2) Используйте функцию separate(), чтобы разделить 
-# столбец с именем и фамилией на два новых: author, name. 
-# Удалите столбец name
-my_data2 <- my_data |>
-  # ваш код здесь
-
-# (3) Используйте group_by() и summarise(), чтобы узнать,
-# сколько произведений Шекспира и Марлоу хранится в библиотеке Gutenberg
-# новый столбец должен называться n, не делайте сортировку 
-my_data3 <- my_data2 |>
-  # ваш код здесь 
+  filter(gutenberg_author_id %in% c(65, 410)) |> 
+  select(author, title) 
+  
+  # (2) Используйте функцию separate(), чтобы разделить 
+  # столбец с именем и фамилией на два новых: author, name. 
+  # Удалите столбец name
+  my_data2 <- my_data |> separate(author, into = c("author", "name"), sep = ", ") |> 
+    select (-name)
+  
+  # (3) Используйте group_by() и summarise(), чтобы узнать,
+  # сколько произведений Шекспира и Марлоу хранится в библиотеке Gutenberg
+  # новый столбец должен называться n, не делайте сортировку 
+  my_data3 <- my_data2 |>
+    group_by(author) |>
+    summarise(total = n())
